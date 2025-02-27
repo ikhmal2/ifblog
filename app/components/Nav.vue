22c0949a397eca5e7a364b08e690945ec14da8df
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-02-27 16:23:25 +0800

    feat: add navbar
    initial work on the navbar

---
+++ app/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+	<header class="flex justify-between items-center px-8 py-4">
+		<a href="/">
+			<span class="logo">Ikhmal Fahmi</span>
+		</a>
+		<div id="site-header-actions">
+			<Icon name="material-symbols:dark-mode" size="2rem" mode="svg" />
+		</div>
+	</header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+	position: sticky;
+	z-index: 2;
+	top: 0;
+	left: 0;
+	background-color: linear-gradient(to top, transparent 0, var(--top-color) 100%);
+	width: 100dvw;
+}
+</style>
