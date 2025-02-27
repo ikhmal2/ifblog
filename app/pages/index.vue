be2f1019d0555fc61d02f976094abc557c8f9850
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-02-27 16:23:48 +0800

    feat: add index page that is not using md

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<MainLayout>
+		lancau
+	</MainLayout>
+</template>
+
+<script setup lang="ts">
+import MainLayout from "@/layout/MainLayout.vue"
+
+</script>
+
+<style scoped></style>
