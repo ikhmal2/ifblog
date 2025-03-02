b1f5d76e82d69450980f7286dd6a6d74100f2dd0
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-03-02 23:39:48 +0800

    build: set up tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from "nuxt/config";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxt/icon"],
+  modules: ["@nuxt/content", "@nuxt/icon", "@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: "2024-04-03",
-  css: ["./app/assets/css/main.css"],
-  vite: {
-    plugins: [tailwindcss()],
+  tailwindcss: {
+    cssPath: ["./app/assets/css/tailwind.css", { injectPosition: "first" }],
+    config: {},
+    viewer: true,
+    exposeConfig: false,
   },
+  css: ["./app/assets/css/main.css"],
+  compatibilityDate: "2024-04-03",
 });
