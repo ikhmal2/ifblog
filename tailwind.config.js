362f24248332f73b097b579a1c13befaad2bb9c5
Ikhmal Fahmi <MuhammadIkhmalFahmi.binMohdFazly@aia.com>
2025-03-06 10:01:57 +0800

    feat: update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ export default {
 		},
 		extend: {
 			colors: {
-				englishViolet: {
-					'DEFAULT': 'hsla(294, 22%, 25%, 1)'
-				},
-				dimGray: { 'DEFAULT': 'hsla(273, 5%, 42%, 1)' }
+				IFprimary: { 'DEFAULT': 'hsla(294, 22%, 25%, 1)' },
+				IFsecondary: { 'DEFAULT': 'hsla(273, 5%, 42%, 1)' },
+				IFtertiary: { 'DEFAULT': 'hsla(94, 19%, 67%, 1)' },
+				IFcta: { 'DEFAULT': 'hsla(103, 65%, 75%, 1)' },
 			}
 		},
 	},
