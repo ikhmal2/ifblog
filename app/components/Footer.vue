e7c4b8b4e812fe38c374f73ccf10ae91f9635a41
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-03-13 00:26:11 +0800

    feat: add footer component

---
+++ app/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer
+    class="container h-32 max-w-full bg-gray-300 p-4 text-center text-gray-700 shadow-md"
+  >
+    <h4>Liking what you read?</h4>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
